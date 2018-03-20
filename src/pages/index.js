9e79afdcefc6f059ba7801d87c9cd4f411ba8098
Phil Alsford <phil.alsford@xero.com>
2018-03-20 22:46:27 +1300

    Working

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,31 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
-const IndexPage = () => (
-  <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
-  </div>
-)
+const IndexPage = ({ data }) => {
+  console.log('data:', data);
+  return (
+    <div>
+      <h1>Hi people</h1>
+      <p>Welcome to your new Gatsby site.</p>
+      <p>Now go build something great.</p>
+      <Link to="/page-2/">Go to page 2</Link>
+      {data.allTrelloCard.edges.map((edge, idx) => {
+        return <p key={idx}>{edge.node.name}</p>
+      })}
+    </div>
+  )
+};
+
+export const query = graphql`
+query AllCards {
+  allTrelloCard(filter: {idBoard: {eq: "59d2bbb9eb383dbdd9dfaff8"}}) {
+    edges {
+      node {
+        id
+        name
+      }
+    }
+  }
+}`;
 
 export default IndexPage
