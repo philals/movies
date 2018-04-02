14d4a4bc3612c03b0504e4a29e8245cc5a85bdf9
Phil Alsford <phil.alsford@xero.com>
2018-04-02 22:14:16 +1200

    Better spacing

--- src/components/MovieGrid/MovieGrid.jsx
+++ src/components/MovieGrid/MovieGrid.jsx
@@ -1,21 +1,5 @@
-import { GridList, Grid, Cell, Card, Media, CardText } from 'react-md';
+import { Grid, Cell, CardText } from 'react-md';
 import React, { Component } from "react";
-
-import PropTypes from 'prop-types'
-import Helmet from 'react-helmet'
-
-
-// Card[] {
-// imageUrl
-// name
-// }
-
-{/* <Grid className="grid-example">
-    <Cell size={6} tabletSize={8}>6 (8 tablet)</Cell>
-    <Cell size={4} tabletSize={6}>4 (6 tablet)</Cell>
-    <Cell size={2} phoneSize={4}>2 (4 phone)</Cell>
-</Grid> */}
-
 
 class MovieGrid extends Component {
     constructor(props) {
@@ -24,13 +8,12 @@ class MovieGrid extends Component {
     render() {
 
         return (
-
-            <Grid container="pictures" size={1} component="section" >
+            <Grid container="pictures" component="section" >
                 {this.props.movies.map((card, key) => {
                     return (
-                        <Cell key={key} style={{ minWidth: 380 }} >
+                        <Cell key={key} style={{ minWidth: 380, marginLeft: 'auto', marginRight: 'auto' }} >
                             <div style={{ 'textAlign': 'center' }}>
-                                <img className={'md-cell--middle'} style={{ 'margin': 'auto', 'minHeight': 445 }} src={card.imageUrl} alt="Something" />
+                                <img className={'md-cell--middle'} style={{ 'margin': 'auto', 'minHeight': 445 }} src={card.imageUrl} alt={card.name} />
                             </div>
                             <CardText expandable={false}>{card.name}</CardText>
                         </Cell>
